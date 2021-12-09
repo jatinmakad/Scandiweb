810b6f6ba8fb460c4a14615a3e7a2f225b992283
jatinmakad <38394914+jatinmakad@users.noreply.github.com>
2021-12-09 20:00:20 +0530

    update

--- src/components/HeaderList.js
+++ src/components/HeaderList.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import styled from 'styled-components';
-import { filteredProduct } from '../slice/fetchSlice';
-import "../styles/Header.css"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { filteredProduct } from "../slice/fetchSlice";
+import "../styles/Header.css";
 class HeaderList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          category: "all",
-        };
-      }
-      handler = (id) => {
-        this.setState({
-          category: id,
-        });
-      };
-    render() {
-        return (
-            <Headerlist>
-            <li
-              onClick={() => {
-                this.props.category("all");
-                this.handler("all");
-              }}
-              className={this.state.category === "all" ? "active" : ""}
-            >
-              All
-            </li>
-            <li
-              onClick={() => {
-                this.props.category("clothes");
-                this.handler("clothes");
-              }}
-              className={this.state.category === "clothes" ? "active" : ""}
-            >
-              Clothes
-            </li>
-            <li
-              onClick={() => {
-                this.props.category("tech");
-                this.handler("tech");
-              }}
-              className={this.state.category === "tech" ? "active" : ""}
-            >
-              Tech
-            </li>
-          </Headerlist>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: "all",
+    };
+  }
+  handler = (id) => {
+    this.setState({
+      category: id,
+    });
+  };
+  render() {
+    return (
+      <Headerlist>
+        <li
+          onClick={() => {
+            this.props.category("all");
+            this.handler("all");
+          }}
+          className={this.state.category === "all" ? "active" : ""}
+        >
+          All
+        </li>
+        <li
+          onClick={() => {
+            this.props.category("clothes");
+            this.handler("clothes");
+          }}
+          className={this.state.category === "clothes" ? "active" : ""}
+        >
+          Clothes
+        </li>
+        <li
+          onClick={() => {
+            this.props.category("tech");
+            this.handler("tech");
+          }}
+          className={this.state.category === "tech" ? "active" : ""}
+        >
+          Tech
+        </li>
+      </Headerlist>
+    );
+  }
 }
 const Headerlist = styled.ul`
   display: flex;
@@ -69,8 +69,8 @@ const Headerlist = styled.ul`
   }
 `;
 const mapDispatchToProps = (dispatch) => {
-    return {
-      category: (id) => dispatch(filteredProduct(id)),
-    };
+  return {
+    category: (id) => dispatch(filteredProduct(id)),
   };
-export default connect(null,mapDispatchToProps)(HeaderList)
+};
+export default connect(null, mapDispatchToProps)(HeaderList);
